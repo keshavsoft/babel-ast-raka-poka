3090ef9b12bfb53631cd4494b04913a6e1eef5b4
TECH-SOLOMAN <solomanmeriga33@gmail.com>
2026-08-13 10:48:26 +0530

    feat: add v2 AST node handling

---
+++ v2/app.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import { router as routerFromdayBook } from './dayBook/end-points.js';
+
+const router1 = express.router();
---
+++ v2/run.js
@@ -0,0 +1,87 @@
+import fs from "node:fs";
+import { parse } from "@babel/parser";
+
+
+const filePath = "./app.js";
+
+
+const source = fs.readFileSync(filePath, "utf8");
+
+
+const ast = parse(source, {
+    sourceType: "module"
+});
+
+
+// for (const node of ast.program.body) {
+//     console.log(node);
+// };
+
+
+
+// for (const node of ast.program.body) {
+//     console.log({
+//         type: node.type,
+//         start: node.start,
+//         end: node.end,
+//         text: source.slice(node.start, node.end),
+//         lineType: node.specifiers[0].type,
+//         raka: node.source?.value,
+//         poka: node.specifiers[0].local.name,
+//         importedName: node.specifiers[0].imported?.name
+//     });
+// };
+
+
+for (const node of ast.program.body) {
+
+    switch (node.type) {
+
+        case "ImportDeclaration":
+
+            console.log({
+                type: node.type,
+                start: node.start,
+                end: node.end,
+                text: source.slice(node.start, node.end),
+                lineType: node?.type,
+                raka: node.source?.value,
+                poka: node.specifiers[0]?.local?.name,
+                importedName: node.specifiers[0]?.imported?.name
+            });
+
+            break;
+
+
+        case "VariableDeclaration":
+
+            console.log({
+                type: node.type,
+                start: node.start,
+                end: node.end,
+                text: source.slice(node.start, node.end),
+                lineType: node?.type,
+                raka: node.source?.value,
+                poka: node.declarations[0]?.id?.name,
+                importedName: node.specifiers?.[0]?.imported?.name
+            });
+
+            break;
+
+
+        default:
+
+            console.log({
+                type: node.type,
+                start: node.start,
+                end: node.end,
+                text: source.slice(node.start, node.end),
+                lineType: node?.type,
+                raka: node.source?.value,
+                poka: node.declarations?.[0]?.id?.name,
+                importedName: node.specifiers?.[0]?.imported?.name
+            });
+
+            break;
+    }
+}
