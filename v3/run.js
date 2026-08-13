739299d1499f6db2503aab20d832d6b30aa6767e
keshavsoft <nknnkumar@yahoo.com>
2026-08-13 10:53:29 +0530

    v3

---
+++ v3/run.js
@@ -0,0 +1,66 @@
+import fs from "node:fs";
+import { parse } from "@babel/parser";
+
+
+const filePath = "./app.js";
+
+
+const source = fs.readFileSync(filePath, "utf8");
+
+
+const ast = parse(source, {
+    sourceType: "module"
+});
+
+for (const node of ast.program.body) {
+
+    switch (node.type) {
+
+        case "ImportDeclaration":
+
+            console.log({
+                type: node.type,
+                start: node.start,
+                end: node.end,
+                text: source.slice(node.start, node.end),
+                lineType: node?.type,
+                raka: node.source?.value,
+                poka: node.specifiers[0]?.local?.name,
+                importedName: node.specifiers[0]?.imported?.name
+            });
+
+            break;
+
+
+        case "VariableDeclaration":
+
+            console.log({
+                type: node.type,
+                start: node.start,
+                end: node.end,
+                text: source.slice(node.start, node.end),
+                lineType: node?.type,
+                raka: node.source?.value,
+                poka: node.declarations[0]?.id?.name,
+                importedName: node.specifiers?.[0]?.imported?.name
+            });
+
+            break;
+
+
+        default:
+
+            console.log({
+                type: node.type,
+                start: node.start,
+                end: node.end,
+                text: source.slice(node.start, node.end),
+                lineType: node?.type,
+                raka: node.source?.value,
+                poka: node.declarations?.[0]?.id?.name,
+                importedName: node.specifiers?.[0]?.imported?.name
+            });
+
+            break;
+    }
+}
