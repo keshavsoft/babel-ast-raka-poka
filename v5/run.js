286d60e86513648536b7fae2b1bc278ef922c1eb
keshavsoft <nknnkumar@yahoo.com>
2026-08-13 12:11:23 +0530

    v5

---
+++ v5/run.js
@@ -0,0 +1,33 @@
+import fs from "node:fs";
+import { parse } from "@babel/parser";
+
+import {
+    processAst
+} from "./astHandlers.js";
+
+
+const filePath =
+    "./jsFiles/app.js";
+
+
+const source =
+    fs.readFileSync(
+        filePath,
+        "utf8"
+    );
+
+
+const ast =
+    parse(source, {
+        sourceType: "module"
+    });
+
+
+const results =
+    processAst(
+        ast,
+        source
+    );
+
+
+console.log(results);
