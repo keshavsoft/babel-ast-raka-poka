11c72fcf92d9f4cf6738492e173a516d5823a2a9
keshavsoft <nknnkumar@yahoo.com>
2026-08-13 10:30:58 +0530

    v2 start

--- app1.js
+++ app1.js
@@ -1,4 +1,4 @@
 import express from 'express';
 import { router as routerFromdayBook } from './dayBook/end-points.js';
 
-const router1 = express.router();
+const router = express.router();
--- run.js
+++ run.js
@@ -9,11 +9,33 @@ const ast = parse(source, {
     sourceType: "module"
 });
 
+// for (const node of ast.program.body) {
+//     console.log(node);
+// };
+
+
+// for (const node of ast.program.body) {
+//     console.log({
+//         type: node.type,
+//         start: node.start,
+//         end: node.end,
+//         text: source.slice(node.start, node.end),
+//         lineType: node.specifiers[0].type,
+//         raka: node.source?.value,
+//         poka: node.specifiers[0].local.name,
+//         importedName: node.specifiers[0].imported?.name
+//     });
+// };
+
 for (const node of ast.program.body) {
     console.log({
         type: node.type,
         start: node.start,
         end: node.end,
-        text: source.slice(node.start, node.end)
+        text: source.slice(node.start, node.end),
+        lineType: node?.type,
+        raka: node.source?.value,
+        poka: node.declarations[0]?.id?.name,
+        importedName: node.specifiers[0].imported?.name
     });
 };
